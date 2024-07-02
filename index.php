338667da5fde9de3d49368d643a3026cd89ea1da
Андрей Семенов <metasea333@gmail.com>
2024-07-02 21:17:15 +0300

    some new changes - menu and php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include "./header.php"?>
+    <div style="background-image: url(./images/ban-image.png);" class="ban-image">
+        <section   class="ban-image__text">
+            <h1 class="ban-image__text--title">Возвращение этнографа</h1>
+            <p class="ban-image__text--info">Сегодня с Проксимы вернулась этнографическая экспедиция Джона Голдрама.</p>
+        </section>
+    </div>    
+    <div class="main-info">
+        <section class="main-info__title">
+                <h1 class="main-info__title--text">Новости</h1>
+        </section>
+
+        <section class="main-info__block">
+            <ul class="main-info__block--news">
+                <li class="main-info__block--date"><p>11.06.2412</p></li>
+                <li class="main-info__block--title"><h2>Возвращение этнографа</h2></li>
+                <li class="main-info__block--text"><p>Cегодня с Проксимы вернулась этнографическая экспедиция Джона Голдрама.</p></li>
+                <li class="main-info__block--button"><button class="main-info__block--button--text">ПОДРОБНЕЕ<div id="arrow-1"></div></button></li>
+            </ul>
+            <ul class="main-info__block--news">
+                <li class="main-info__block--date"><p>11.06.2412</p></li>
+                <li class="main-info__block--title"><h2>Несчастный случай с известной светской дивой</h2></li>
+                <li class="main-info__block--text"><p class="main-info__block--text--not-selected">Светская дива Алиса Уткина попала под троллейбус, пытаясь уйти от преследования.</p></li>
+                <li class="main-info__block--button"><button class="main-info__block--button--text">ПОДРОБНЕЕ<div id="arrow-1"></div></button></li>
+            </ul>
+        </section>
+
+        <section class="main-info__block">
+            <ul class="main-info__block--news">
+                <li class="main-info__block--date"><p>26.05.2412</p></li>
+                <li class="main-info__block--title"><h2>Папа Римский совершил визит в систему Альфы Центавра</h2></li>
+                <li class="main-info__block--text"><p class="main-info__block--text--not-selected">Папа Римский Жан-Клод XIV посетил с апостольским визитом систему Альфы Центавра. Он принял участие в ряде благотворительных мероприятий и совершил богослужение в соборе Ван-Дамм-де-Ури.</p></li>
+                <li class="main-info__block--button"><button class="main-info__block--button--text">ПОДРОБНЕЕ<div id="arrow-1"></div></button></li>
+            </ul>
+            <ul class="main-info__block--news">
+                <li class="main-info__block--date"><p>20.05.2412</p></li>
+                <li class="main-info__block--title"><h2>На чемпионате по пустотной гребле победила команда с Фобоса</h2></li>
+                <li class="main-info__block--text"><p class="main-info__block--text--not-selected">В поясе астероидов на проходившем в последние выходные этапе системного чемпионата по пустотной гребле команда с Фобоса одержала убедительную победу.</p></li>
+                <li class="main-info__block--button"><button class="main-info__block--button--text">ПОДРОБНЕЕ<div id="arrow-1"></div></button></li>
+            </ul>
+        </section>
+    </div>
+<?php include "./footer.php"?>
---
+++ task_1-7.php
@@ -0,0 +1,17 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<?php 
+	$array=['abcde']
+	foreach($array as $item){
+		echo $item;
+	}
+?>
+</body>
+</html>
+
